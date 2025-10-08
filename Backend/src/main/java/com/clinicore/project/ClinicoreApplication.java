8219aa72f8202372a726a2532703f7ba5971e3fd
hyemdanu <hoedison2003@gmail.com>
2025-10-07 23:15:44 -0700

    Initializing Project

---
+++ Backend/src/main/java/com/clinicore/project/ClinicoreApplication.java
@@ -0,0 +1,13 @@
+package com.clinicore.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicoreApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/clinicore/project/ClinicoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicore.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
