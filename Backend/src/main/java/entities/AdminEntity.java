16eb180e29a9ea98f508df3527873e6463040b44
hyemdanu <hoedison2003@gmail.com>
2025-10-15 18:36:32 -0700

    Finished AdminEntity.java

---
+++ Backend/src/main/java/entities/AdminEntity.java
@@ -0,0 +1,22 @@
+package entities;
+
+// imports
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+// annotations
+@Entity
+@Table(name = "admin")
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class AdminEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
