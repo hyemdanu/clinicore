fbf93f5cfb11a5d14a9d6b082652760cd8c59b09
winniwu07 <winniwu@csus.edu>
2025-10-16 23:53:04 -0700

    Resident entity javax --> jakarta

--- Backend/src/main/java/com/clinicore/project/entity/Resident.java
+++ Backend/src/main/java/com/clinicore/project/entity/Resident.java
@@ -1,9 +1,9 @@
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
