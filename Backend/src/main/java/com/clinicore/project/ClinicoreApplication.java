ab07e376f6b2db26746290d88789a1d008ba86fe
hyemdanu <hoedison2003@gmail.com>
2025-10-16 20:43:45 -0700

    modified to fix connection to database

--- Backend/src/main/java/com/clinicore/project/ClinicoreApplication.java
+++ Backend/src/main/java/com/clinicore/project/ClinicoreApplication.java
@@ -8,6 +8,10 @@ public class ClinicoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClinicoreApplication.class, args);
+
+        System.out.println("DB_URL = " + System.getenv("DB_URL"));
+        System.out.println("DB_USER = " + System.getenv("DB_USER"));
+        System.out.println("DB_PASS = " + System.getenv("DB_PASS"));
     }
 
 }
