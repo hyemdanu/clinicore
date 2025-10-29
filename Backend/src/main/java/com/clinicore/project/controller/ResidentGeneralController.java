2675f1eff4a7b9a4996c1cd9297cd12e7d501c6e
aims <aimsvillano@gmail.com>
2025-10-28 20:14:12 -0700

    added /list mapping to list all residents available (used in caregiver/resident dashboards)

--- Backend/src/main/java/com/clinicore/project/controller/ResidentGeneralController.java
+++ Backend/src/main/java/com/clinicore/project/controller/ResidentGeneralController.java
@@ -13,8 +13,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 // annotations
 @RestController
@@ -34,9 +37,44 @@ public class ResidentGeneralController {
     }
 
     /**
+     * Get a list of all residents (for caregiver to browse/select)
+     * Returns basic info only - just enough to display in a list
+     * 
+     * @return JSON array containing basic info for all residents
+     */
+    @GetMapping("/list")
+    public ResponseEntity<?> getAllResidents() {
+        List<UserProfile> allUsers = userProfileRepository.findAll();
+        List<Map<String, Object>> residentList = new ArrayList<>();
+        
+        for (UserProfile user : allUsers) {
+            // Check if this user is also a resident
+            Optional<Resident> resident = residentRepository.findById(user.getId());
+            
+            if (resident.isPresent()) {
+                // This user is a resident, add to list
+                Map<String, Object> residentInfo = new LinkedHashMap<>();
+                residentInfo.put("id", user.getId());
+                residentInfo.put("firstName", user.getFirstName());
+                residentInfo.put("lastName", user.getLastName());
+                
+                residentList.add(residentInfo);
+            }
+        }
+        
+        return ResponseEntity.ok(residentList);
+    }
+
+    /**
      * This will take the ID connected to the resident from userProfile and then pulls the general information
      * (like name) and then combines it with the resident information table (like emergency contact).
-     * Returns a JSON object containing both user profile and resident info
+     * 
+     * Used by:
+     * - Residents to view their own information
+     * - Caregivers/Admins to view a selected resident's detailed information
+     * 
+     * @param userProfileId The ID of the user/resident to retrieve
+     * @return JSON object containing both user profile and resident info
      */
     @GetMapping("/{userProfileId}/general")
     public ResponseEntity<?> getResidentGeneral(@PathVariable Long userProfileId) {
@@ -61,6 +99,8 @@ public class ResidentGeneralController {
         residentsResult.put("userProfileId", userProfileId);
 
         // basic user profile details
+        residentsResult.put("firstName", up.getFirstName());
+        residentsResult.put("lastName", up.getLastName());
         residentsResult.put("username", up.getUsername());
         residentsResult.put("gender", up.getGender());
         residentsResult.put("birthday", up.getBirthday());
