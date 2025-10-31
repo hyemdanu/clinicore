b18f4850fb506e690d5d0657fc0e5bd356aae75a
adrianpxrez <axrianjosh@gmail.com>
2025-10-31 04:44:18 -0700

    created Document Service file

---
+++ Backend/src/main/java/com/clinicore/project/service/DocumentService.java
@@ -0,0 +1,14 @@
+package com.clinicore.project.service;
+
+import com.clinicore.project.entity.*;
+import com.clinicore.project.repository.*;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+public class DocumentService {
+
+    private final DocumentsRepository documentsRepository;
+    private final UserProfileRepository userProfileRepository;
+
+}
