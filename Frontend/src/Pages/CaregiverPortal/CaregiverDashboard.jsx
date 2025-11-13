a8a3ae652781edd5a8bd0b32c4ff3b3ae994eccd
adrianpxrez <adrianperez3@csus.edu>
2025-11-12 21:33:05 -0800

    Added Medication sections and labels

--- Frontend/src/Pages/CaregiverPortal/CaregiverDashboard.jsx
+++ Frontend/src/Pages/CaregiverPortal/CaregiverDashboard.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import "./css/caregiver.css";
 
 export default function CaregiverDashboard() {
-     return (
+    return (
         <div className="caregiver-page">
             <div className="box">
                 {/* Top blue header */}
                 <div className="rectangle">
                     <div className="caregiver-dashboard-title">Dashboard</div>
                 </div>
-
+                {/* Medication Tasks section */}
+                <section className="medication-section">
+                    <div className="medication-header">
+                        <div>
+                            <h2 className="medication-label">Medication Tasks</h2>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
     );
 }
 
