25835f81c9692d823b22c063f73bd16898f467db
adrianpxrez <adrianperez3@csus.edu>
2025-11-13 02:27:25 -0800

    Added progress bar /w fill and percentage

--- Frontend/src/Pages/CaregiverPortal/CaregiverDashboard.jsx
+++ Frontend/src/Pages/CaregiverPortal/CaregiverDashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./css/caregiver.css";
 
 export default function CaregiverDashboard() {
+    const progressPercent = 65;
     return (
         <div className="caregiver-page">
             <div className="box">
@@ -9,12 +10,28 @@ export default function CaregiverDashboard() {
                 <div className="rectangle">
                     <div className="caregiver-dashboard-title">Dashboard</div>
                 </div>
+
                 {/* Medication Tasks section */}
                 <section className="medication-section">
                     <div className="medication-header">
                         <div>
                             <h2 className="medication-label">Medication Tasks</h2>
                         </div>
+
+                        <div className="progress-summary">
+                            <span className="progress-title">Progress</span>
+                            <span className="progress-value">
+                                {progressPercent}%
+                            </span>
+                        </div>
+                    </div>
+
+                    {/* Progress bar */}
+                    <div className="medication-progress-container">
+                        <div
+                            className="medication-progress"
+                            style={{ width: `${progressPercent}%` }}
+                        />
                     </div>
                 </section>
             </div>
