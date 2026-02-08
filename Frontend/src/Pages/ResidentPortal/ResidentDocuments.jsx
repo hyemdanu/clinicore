278ff79260d662ec0c0dc4537ca88e58144f70d3
adrianpxrez <axrianjosh@gmail.com>
2026-02-07 17:35:51 -0800

    Created base files for resident documents

---
+++ Frontend/src/Pages/ResidentPortal/ResidentDocuments.jsx
@@ -0,0 +1,20 @@
+// src/pages/ResidentDocuments.jsx
+import "./css/ResidentDocuments.css";
+
+export default function ResidentDocuments() {
+  return (
+    <div className="resident-documents">
+      <h1 className="resident-documents__title">
+        Resident Documents
+      </h1>
+
+      <p className="resident-documents__subtitle">
+        Upload, view, and manage resident files
+      </p>
+
+      <div className="resident-documents__content">
+        {/* content goes here */}
+      </div>
+    </div>
+  );
+}
