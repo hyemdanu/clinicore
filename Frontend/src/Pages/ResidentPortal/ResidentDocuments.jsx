33ea2548b230c0dd0898093778cb27a582a058f2
adrianpxrez <axrianjosh@gmail.com>
2026-02-07 18:22:21 -0800

    created base box and title

--- Frontend/src/Pages/ResidentPortal/ResidentDocuments.jsx
+++ Frontend/src/Pages/ResidentPortal/ResidentDocuments.jsx
@@ -1,20 +1,24 @@
-// src/pages/ResidentDocuments.jsx
-import "./css/ResidentDocuments.css";
+import React from "react";
+import "./css/residentDocuments.css";
 
 export default function ResidentDocuments() {
-  return (
-    <div className="resident-documents">
-      <h1 className="resident-documents__title">
-        Resident Documents
-      </h1>
+    return (
+        <div className="resident-documents-page">
+            <div className="box">
+                <div className="rectangle">
+                    <div className="resident-documents-title">
+                        Documents
+                    </div>
+                </div>
 
-      <p className="resident-documents__subtitle">
-        Upload, view, and manage resident files
-      </p>
-
-      <div className="resident-documents__content">
-        {/* content goes here */}
-      </div>
-    </div>
-  );
+                <section className="documents-section">
+                    {/* content coming next */}
+                </section>
+            </div>
+        </div>
+    );
 }
+
+
+
+
